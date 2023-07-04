ff2bd0e77c0a7f02c17912070468971b13e5cef0
hypergavs <hypergavs@gmail.com>
2023-07-05 01:00:37 +0800

    s

--- src/layouts/dashboard/DashboardLayout.tsx
+++ src/layouts/dashboard/DashboardLayout.tsx
@@ -1,8 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonRouterOutlet, IonToolbar } from "@ionic/react";
+import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonTitle, IonToolbar } from "@ionic/react";
 import { FC, useContext } from "react";
 import DashboardRoutes from "../../routes/DashboardRoutes";
 import UserContextProvider, { UserContext } from "../../contexts/userContext";
-import DashboardHeader from "../../pages/dashboard/DashboardHeader";
+import { mail, menuOutline } from "ionicons/icons";
+import logoFull from "../../assets/images/logo-full.png";
+import { useHistory } from "react-router";
+import { Logout } from "../../requests/auth.request";
+import '../../styles/Dashboard.scss'
 
 export interface iProps {
 
@@ -10,16 +14,39 @@ export interface iProps {
 export const DashboardLayout: FC<iProps> = (props): JSX.Element => {
 
     const { user } = useContext(UserContext)
+    const navigation = useHistory();
+
+    const processLogout = async () => {
+        try {
+            const logoutRequest = await Logout()
+            localStorage.removeItem("authToken")
+            navigation.push("/login")
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
     return (
-            
-        <IonPage id="dashboard">
+        
+        <div id="dashboard">
+            <IonPage id="dashboard-header-content">
                 {!!user?.email == false ? (
                     <IonContent>Unauthenticated!, Login first!</IonContent>
                     ): (
-                        <>
-                        
-                        <DashboardHeader />
+                    <>
+                        <IonHeader className='ion-padding-horizontal'>
+                            <IonToolbar style={{height: "70px", display: "flex"}}>
+                                    <IonMenuToggle slot='end' >
+                                        <IonButton fill='solid' color={"primary"}>
+                                            <IonIcon slot='icon-only' icon={menuOutline}></IonIcon>
+                                        </IonButton>
+                                    </IonMenuToggle>
+                                <IonTitle>
+                                    <img src={logoFull} className='logo'  height={50}/>
+                                </IonTitle>
+                            </IonToolbar>
+                        </IonHeader>
                         <IonContent>
                             <IonRouterOutlet>
                                 <DashboardRoutes />
@@ -27,8 +54,35 @@ export const DashboardLayout: FC<iProps> = (props): JSX.Element => {
                         </IonContent>
                     </>
                 )}
-                
             </IonPage>
+            <IonMenu contentId='dashboard-header-content' className='dashboard-navigation-content' type='push'>
+                <div className='logo-wrap'>
+                    <img src={logoFull} className='logo' />
+                </div>
+                <IonContent className='ion-padding'>
+                    <IonList>
+                        <IonItem lines="full" href="/dashboard">
+                            <IonLabel slot='end' color={'primary'} className='dashboard-navigation-link'>Dashboard</IonLabel>
+                        </IonItem>
+                        <IonItem lines="full" href="/dashboard">
+                            <IonLabel slot='end' color={'primary'} className='dashboard-navigation-link'>Dashboard</IonLabel>
+                        </IonItem>
+                        <IonItem lines="full" href="/dashboard">
+                            <IonLabel slot='end' color={'primary'} className='dashboard-navigation-link'>Dashboard</IonLabel>
+                        </IonItem>
+                        <IonItem lines='full' onClick={processLogout}>
+                            <IonLabel slot='end' color={'primary'} className='dashboard-navigation-link'>
+                                Log out
+                            </IonLabel>
+                        </IonItem>
+                        <IonButton className='ion-margin-top' fill='solid' shape='round' expand="full" style={{textTransform: "lowercase"}}>
+                            <IonIcon slot="start" icon={mail}></IonIcon>
+                            support@aimalls.app
+                        </IonButton>
+                    </IonList>
+                </IonContent>
+            </IonMenu>
+        </div>
     )
 };
 export default DashboardLayout;
